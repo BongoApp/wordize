915443ea6cfbec38810684148975fc9440d3ed6c
Fulano2141 <macuto2142dt@gmail.com>
2021-06-13 16:26:13 -0400

    dockerfile and fixed root

--- words/src/main/java/com/bo/wordize/api/HelloWorldApi.java
+++ words/src/main/java/com/bo/wordize/api/HelloWorldApi.java
@@ -4,7 +4,6 @@ import com.bo.wordize.bl.HelloWorldBl;
 import com.bo.wordize.model.HelloWorld;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
