dc4b58e4a39c0786e693506109185325dfde9df0
macut <macuto2142dt@gmail.com>
2021-06-14 22:13:50 -0400

    fake test for jenkins

--- words/src/test/java/com/bo/wordize/WordizeApplicationTests.java
+++ words/src/test/java/com/bo/wordize/WordizeApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bo.wordize;
 
+import com.bo.wordize.model.Word;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class WordizeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Word word = null;
+        Assert.isNull(word);
+    }
 
 }
